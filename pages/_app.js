071667cb68c58d130e32fa110ff8d076e4b07b9d
Fernando Abellan <faccuo@gmail.com>
2021-02-19 11:47:29 +0100

    Added more styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'open-iconic/font/css/open-iconic-bootstrap.min.css'
 import Head from "next/head";
 
 function Application({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
-import {Button, Form, FormControl, Image, Jumbotron, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import {Head} from "next/document";
+import {Button, Form, Image, Jumbotron, Navbar} from "react-bootstrap";
 
 export default function Home() {
   return (
     <div className="container">
-      <Navbar bg="white">
+      <Navbar bg="white" className="justify-content-between">
         <Navbar.Brand href="#">
           <img src="logo.png" height="30"/>
         </Navbar.Brand>
+        <Form inline>
+          <Button variant="primary"><span className="oi oi-plus" aria-hidden="true"></span> Crear
+            porra</Button>
+        </Form>
       </Navbar>
       <Jumbotron className="text-center">
         <h1 className="jumbotron-heading">Crea porras gratis en segundos</h1>
         <p className="lead text-muted">Participa y comparte tu pronóstico sin instalar ninguna app. No requiere
           registro.</p>
-        <Image src="landing.png" fluid/>
+        <Image src="landing.png" fluid className="img-landing"/>
+        <Button variant="primary" className="btn-start">Empezar</Button>
       </Jumbotron>
     </div>
   )
