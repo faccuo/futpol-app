ac44a5259b29ae10e836c396b3bbb04d1f664506
Fernando Abellan <faccuo@gmail.com>
2021-06-05 01:56:14 +0200

    Migrate index to material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,53 @@
-import '@styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'open-iconic/font/css/open-iconic-bootstrap.min.css'
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { AddBox } from '@material-ui/icons';
+import '@styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { motion } from "framer-motion";
 import Head from "next/head";
-import {Button, Form, Navbar} from "react-bootstrap";
-import Link from "next/link";
-import {motion} from "framer-motion";
+import { makeStyles } from '@material-ui/core/styles';
+import Link from 'next/link';
 
-function Application({Component, pageProps, router}) {
+
+const useStyles = makeStyles(theme => ({
+  logo: {
+    flexGrow: 1,
+  },
+}));
+
+function Application({ Component, pageProps, router }) {
+  const classes = useStyles();
+
   return (
     <>
       <Head>
         <title>Futpol - porras gratis</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </Head>
-      <div className="container">
-        <Navbar bg="white" className="justify-content-between">
-          <Link href="/">
-            <Navbar.Brand href="#">
-              <img src="logo.png" height="30"/>
-            </Navbar.Brand>
-          </Link>
-          <Form inline>
-            <Link href="/create">
-              <Button variant="primary"><span className="oi oi-plus" aria-hidden="true"></span></Button>
+      <AppBar position="static" color="default" elevation={0} color="inherit">
+        <Toolbar>
+          <div className={classes.logo}>
+            <Link href="/" passHref>
+              {/* <img src="logo.png" height="30" /> */}
+              <Button startIcon={<img src="logo.png" height="30" />} style={{ backgroundColor: 'transparent' }} disableRipple/> 
             </Link>
-          </Form>
-        </Navbar>
-        <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
-          pageInitial: {
-            opacity: 0
-          },
-          pageAnimate: {
-            opacity: 1
-          },
-        }}>
-          <Component {...pageProps} />
-        </motion.div>
-      </div>
+          </div>
+          <Link href="/create" passHref>
+            <IconButton color="primary">
+              <AddBox fontSize="large" />
+            </IconButton>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
+        pageInitial: {
+          opacity: 0
+        },
+        pageAnimate: {
+          opacity: 1
+        },
+      }}>
+        <Component {...pageProps} />
+      </motion.div>
     </>
   )
 }
